1b2ec4a0124c28387bbad11f44429df2cf7d6bb9
ColdProgrammer <timetoprogram@gmail.com>
2017-11-03 11:11:01 -0500

    Update final.m
    Added comments and realigned the code

--- final.m
+++ final.m
@@ -11,34 +11,33 @@ filePattern = fullfile(myFolder, '*.tiff');
 %matrix to store all hamming distance scores
 matrix =eye(220);
 
-
- % Change to whatever pattern you need.
+% Change to whatever pattern you need.
 theFiles = dir(filePattern);
 for k = 1: length(theFiles)
-    baseFileName = theFiles(k).name;
+  baseFileName = theFiles(k).name;
   fullFileName = fullfile(myFolder, baseFileName);
   fprintf(1, 'First Loop: Now reading%s\n', fullFileName);
- 
-    
-   [template1,mask1]=createiristemplate(fullFileName);   
-%Change to gallery folder to compare 1 probe to all the gallery photos
-myFolder='C:\Users\boltz\Desktop\biometrics\iriscode\LG2200-2008-gallery';
-filePattern = fullfile(myFolder, '*.tiff');
-theFiles = dir(filePattern);
- for j = 1: length(theFiles) 
+  [template1,mask1]=createiristemplate(fullFileName);   
   
-   baseFileName = theFiles(j).name;
-  fullFileName = fullfile(myFolder, baseFileName);
-  fprintf(1, 'Second Loop: Now reading%s\n', fullFileName);
-   [template2, mask2] = createiristemplate(fullFileName);
-  matrix(k,j) = gethammingdistance(template1, mask1, template2, mask2, 1);
-
- end 
- %Change back to probe
- myFolder='C:\Users\boltz\Desktop\biometrics\iriscode\LG-2200-2010-probe';
- filePattern = fullfile(myFolder, '*.tiff');
- theFiles = dir(filePattern);
+  %Change to gallery folder to compare 1 probe to all the gallery photos
+  myFolder='C:\Users\boltz\Desktop\biometrics\iriscode\LG2200-2008-gallery';
+  filePattern = fullfile(myFolder, '*.tiff');
+  theFiles = dir(filePattern);
+  for j = 1: length(theFiles) 
+    baseFileName = theFiles(j).name;
+    fullFileName = fullfile(myFolder, baseFileName);
+    fprintf(1, 'Second Loop: Now reading%s\n', fullFileName);
+    [template2, mask2] = createiristemplate(fullFileName);
+    
+    %The matchscores are saved in a matrix which would be used for final calculations
+    matrix(k,j) = gethammingdistance(template1, mask1, template2, mask2, 1);
+  end 
+  %Change back to probe
+  myFolder='C:\Users\boltz\Desktop\biometrics\iriscode\LG-2200-2010-probe';
+  filePattern = fullfile(myFolder, '*.tiff');
+  theFiles = dir(filePattern);
   fprintf(1, 'First Loop: Now reading changed path %s\n', fullFileName);
 end
+%Saving the matrix
 matrix2200=matrix(1:220,1:88);
 
